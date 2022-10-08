1e34123f045432d25d8c3a1f6c7f7b6456a0b80c
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-08 15:02:38 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
-import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
+import { Outlet, ScrollRestoration, useNavigation } from 'react-router-dom';
 import styles from './App.module.scss';
 
 function App() {
-  const navigation = useLocation();
+  const navigation = useNavigation();
 
   useEffect(() => {
     console.log(navigation);
@@ -13,6 +13,12 @@ function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
+      { {navigation.state === "loading" && (
+        <h1>
+          Loading{" "}
+          {navigation.location.pathname === "/" && <strong>Homepage</strong>}{" "}
+        </h1>
+      )}}
       <div className="flex-fill">
         <Outlet />
       </div>
